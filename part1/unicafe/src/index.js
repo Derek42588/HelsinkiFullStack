0f4c1e2aa8a4d4af68a1c9ca3128c002cf0750cd
Derek42588 <46350852+Derek42588@users.noreply.github.com>
2019-09-15 10:38:37 -0400

    part1 exercise 1.9 -> 1.10
    refactor Statistics component to render multiple Statistic components, where each only displays one specific statistic

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -6,6 +6,17 @@ const Button = (props) => (
         {props.text}
     </button>
 )
+
+const Statistic = (props) => { 
+    if (props.text === "positive") {
+        return (
+            <p>{props.text} {props.value} %</p>
+        )
+    }
+    return (
+    <p>{props.text} {props.value}</p>
+)
+}
 const Statistics = ({good, neutral, bad}) => {
     if (good + neutral + bad === 0) {
         return (
@@ -17,12 +28,12 @@ const Statistics = ({good, neutral, bad}) => {
 
     return (
         <div>
-            <p>good {good}</p>
-            <p>neutral {neutral}</p>
-            <p>bad {bad}</p>
-            <p>all {good + neutral + bad}</p>
-            <p>average {(good - bad)/(good + neutral + bad)}</p>
-            <p>positive {((good)/(good + neutral + bad)) * 100} %</p>
+            <Statistic text ="good" value = {good}/>
+            <Statistic text ="neutral" value = {neutral}/>
+            <Statistic text ="bad" value = {bad}/>
+            <Statistic text ="all" value = {good + neutral + bad}/>
+            <Statistic text ="average" value = {(good - bad)/(good + neutral + bad)}/>
+            <Statistic text ="positive" value = {((good)/(good + neutral + bad)) * 100}/>
         </div>
     )
 
