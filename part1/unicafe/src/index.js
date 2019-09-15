350a2542a6bdd7359b28032de4b9ce57439d5436
Derek42588 <46350852+Derek42588@users.noreply.github.com>
2019-09-15 10:27:28 -0400

    part1 exercise 1.8 -> 1.9
    Refactored Statistics component to conditionally render statistics only after feedback has been given

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -10,12 +10,7 @@ const Statistics = ({good, neutral, bad}) => {
     if (good + neutral + bad === 0) {
         return (
             <div>
-                <p>good {good}</p>
-                <p>neutral {neutral}</p>
-                <p>bad {bad}</p>
-                <p>all {good + neutral + bad}</p>
-                <p>average 0</p>
-                <p>positive 0%</p>
+                <p>No feedback given</p>
             </div>
         )
     }
