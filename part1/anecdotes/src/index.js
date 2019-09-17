9d853c541887ebda2745ae9df74449c69b2663a9
Derek42588 <46350852+Derek42588@users.noreply.github.com>
2019-09-17 07:29:33 -0400

    part1 exercise 1.13 -> 1.14
    implemented display (tracked through state) for the anecdote with the highest vote count

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -11,6 +11,26 @@ const Button = ({handleClick, text}) => {
 )
 }
 
+const Display = ({voteArray, anecdotes, highestRanked}) => {
+
+  
+  // console.log(Math.max(...votesCopy))
+//   let votesCopy = {...voteArray}
+//   var max = votesCopy.reduce(function(a, b) {
+//     return Math.max(a, b)
+// })
+  console.log(anecdotes[highestRanked[0]])
+  console.log(voteArray[highestRanked[0]])
+
+  return (
+    <div>
+      <h1>Anecdote with most Votes</h1>
+      <p>{anecdotes[highestRanked[0]]}</p>
+      <p>has {voteArray[highestRanked[0]]} votes</p>
+    </div>
+  )
+}
+
 
 const App = (props) => {
 
@@ -18,6 +38,8 @@ const App = (props) => {
 
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(voteArray)
+  const [highestRanked, setHighestRanked] = useState([0,0])
+  
 
   const getRandom = (numAnecdotes) => {
     let ran =  Math.floor(Math.random() * Math.floor(numAnecdotes));
@@ -26,9 +48,15 @@ const App = (props) => {
 }
   const voteForAnecdote = (selectedAnecdote) => {
     let voteCopy = {...votes}
+    let currentRank = {...highestRanked}
     voteCopy[selectedAnecdote] += 1
 
     setVotes(voteCopy)
+    
+    if (voteCopy[selectedAnecdote] > currentRank[1]) {
+      let newRank = [selectedAnecdote, voteCopy[selectedAnecdote]]
+      setHighestRanked(newRank)
+    }
 
 
   }
@@ -36,10 +64,12 @@ const App = (props) => {
 
   return (
     <div>
+      <h1>Anecdote of the Day</h1>
       <p>{props.anecdotes[selected]}</p>
       <p>has {votes[selected]} votes</p>
       <Button handleClick = {() => voteForAnecdote(selected)} text = "vote" />
       <Button handleClick = {() => setSelected(getRandom(props.anecdotes.length))} text = "next anecdote" />
+      <Display voteArray = {votes} anecdotes = {props.anecdotes} highestRanked = {highestRanked} />
     </div>
   )
 }
